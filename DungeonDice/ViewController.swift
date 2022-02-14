0c53201201200c66fb02c042af175941a66639e1
Qingwan Cheng <chengqf@bc.edu>
2022-02-14 17:05:19 -0500

    Yes

--- DungeonDice/ViewController.swift
+++ DungeonDice/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        resultLabel.text = ""
     }
 
-
+    @IBAction func diceButtonPressed(_ sender: UIButton) {
+        let rollResult = Int.random(in: 1...sender.tag)
+        resultLabel.text = "You rolled a \(sender.tag)-sided dice and got a \(rollResult)"
+    }
+    
 }
 
